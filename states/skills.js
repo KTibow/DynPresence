b67324ff389f22f6d8b4f799a91dd017e51ba5ec
Kendell R <KTibow@users.noreply.github.com>
2022-02-25 06:23:27 -0800

    ignore amount of xp

--- states/skills.js
+++ states/skills.js
@@ -16,26 +16,25 @@ register("actionBar", (event) => {
   if (!xpMatch) return;
   skillsGained.push({
     skillName: xpMatch[2],
-    amount: new Number(xpMatch[1]),
     time: Date.now(),
   });
 });
 
 const findSkillGrinding = () => {
   // If the player is in Skyblock,
-  // tally up the amount of xp gained per skill,
-  // and return the one with the highest amount.
+  // tally up the number of times the player gained XP,
+  // and return the skill type with the highest amount.
   // (ignore anything not from the past minute)
   if (!isInSkyblock()) return;
   const skills = {};
   for (const skill of skillsGained) {
     if (Date.now() - skill.time > 60000) continue;
-    skills[skill.skillName] = (skills[skill.skillName] || 0) + skill.amount;
+    skills[skill.skillName] = (skills[skill.skillName] || 0) + 1;
   }
 
   const sortedSkills = Object.entries(skills).sort((a, b) => b[1] - a[1]);
   if (sortedSkills.length < 1) return;
-  if (sortedSkills[0][1] < 250) return;
+  if (sortedSkills[0][1] < 20) return;
   return `Grinding ${sortedSkills[0][0]}`;
 };
 
