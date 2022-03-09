84f693892f2d20fac47a7ba02fff1602184d9d3a
Kendell R <KTibow@users.noreply.github.com>
2022-03-08 16:47:54 -0800

    Fix spelling (for firefox, against xkcd)

--- states/queue.js
+++ states/queue.js
@@ -6,7 +6,7 @@ import { getLocraw } from "../details/updateLocraw";
 const isPlayerInQueue = () => {
   const location = getLocraw();
   if (location.server?.includes("lobby")) return false;
-  const isPlayerQueueing = Scoreboard.getLines().some((line) => {
+  const isPlayerQueuing = Scoreboard.getLines().some((line) => {
     const lineText = ChatLib.removeFormatting(line.getName());
     return lineText.includes("Waiting...") || lineText.includes("Starting in");
   });
@@ -23,15 +23,15 @@ const isPlayerInQueue = () => {
       "SPEED_UHC",
       "TNT_GAMES",
     ].includes(location.gametype) &&
-    isPlayerQueueing
+    isPlayerQueuing
   ) {
-    return "Queueing";
+    return "Queuing";
   }
-  if (isPlayerQueueing) {
+  if (isPlayerQueuing) {
     ChatLib.chat(
       `&eThis game has a queue-related message in the scoreboard, but it's not marked as a queable game.&r`
     );
-    ChatLib.chat(`&eFor now, it won't show you as Queueing, just Moving Around.&r`);
+    ChatLib.chat(`&eFor now, it won't show you as Queuing, just Moving Around.&r`);
     ChatLib.chat(
       `&eIf this game is indeed queueable, or not, tell me at &9KTibow#3960&e.&r`
     );
